25587a059cbccc9308007abdebd6d8e5f4b12475
coxKyle <113553410+coxKyle@users.noreply.github.com>
2022-12-06 13:42:42 -0600

    Add files via upload

---
+++ src/chain.js
@@ -0,0 +1,74 @@
+import { ethers } from "ethers";
+import { utils } from 'ethers';
+import IERC20 from "../interfaces/IERC20.json";
+import ISlasher from "../interfaces/ISlasher.json";
+import BCSlasher from "../interfaces/BCSlasher.json";
+
+/* HOW TO SETUP
+    type 'npm run build' and 'npm run serve' in a new terminal to start the webpage
+    adjust variables at EDIT in this document and index.js
+    CTRL + S to save changes
+    click TOGGLE SLASH
+*/
+
+/* EDIT */
+const privateKey = '0x0123456789012345678901234567890123456789012345678901234567890123';
+const alchemyApiKey = ''; //HhxVvFHxxxxxxxxxxxxxxxxxxxxxx
+/* EDIT */
+
+var contractAddress = '0xD47bD12732e8ea7abd047c17Db585E57075a1339'; //with profit safety
+// var contractAddress = '0xE7c79825628Fa84cf7630A449F8628A5dc3c09Ce'; //without profit safety
+var provider = new ethers.providers.AlchemyProvider('matic', alchemyApiKey);
+var wallet = new ethers.Wallet(privateKey, provider);
+var cSLASHER = new ethers.Contract(contractAddress, ISlasher, wallet);
+
+async function approve(tokenIn, weiIn, maxFees) {
+    let t = new ethers.Contract(tokenIn, IERC20, wallet);
+    if (await t.allowance(wallet.address, contractAddress) < weiIn) {
+        await t.approve(contractAddress, ethers.BigNumber.from('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'), {maxFeePerGas: maxFees[0], maxPriorityFeePerGas: maxFees[1]});
+        console.log('APPROVED');
+    }
+}
+
+async function getGasFees() {
+    let response = await (await fetch(new URL('https://gasstation-mainnet.matic.network/v2'))).text();
+    response = response.replace('{"safeLow":', '').replace('"standard":', '').replace('"fast":', '').replaceAll('}', '').replaceAll('{"maxPriorityFee":', '').replaceAll('"maxFee":', ''); 
+    let maxFees = response.split(',');
+    let maxFeePerGas = utils.parseUnits(parseFloat(maxFees[4]).toFixed(9), 'gwei'); //3 will result in less gas and slower transactions
+    let maxPriorityFeePerGas = utils.parseUnits(parseFloat(maxFees[5]).toFixed(9), 'gwei'); //2 will result in less gas and slower transactions
+    if (maxFeePerGas < maxPriorityFeePerGas) {
+        let temp = maxPriorityFeePerGas;
+        maxPriorityFeePerGas = maxFeePerGas;
+        maxFeePerGas = temp;
+    }
+    return [maxFeePerGas, maxPriorityFeePerGas]; //? priority bigger than other
+}
+
+export async function preRun(tokenIn, tokenTrade, weiIn, routerAddress1, routerAddress2) {
+    let maxFees = await getGasFees();
+    await approve(tokenIn, weiIn, maxFees);
+    let gl = await cSLASHER.estimateGas.slash(tokenIn, tokenTrade, weiIn, routerAddress1, routerAddress2, {maxFeePerGas: maxFees[0], maxPriorityFeePerGas: maxFees[1]}); //nonce: await provider.getTransactionCount(wallet.address)
+    let gasFee = gl*maxFees[0];
+    console.log('gas fee: ' + gasFee / 10**18);
+    if (tokenIn == await cSLASHER.getWETH(routerAddress1)) {
+        return gasFee;
+    } else {
+        return await getWeiOut(await cSLASHER.getWETH(routerAddress1), tokenIn, weiIn, routerAddress1);
+    }
+}
+
+export async function slasher(tokenIn, tokenTrade, weiIn, routerAddress1, routerAddress2) {
+    let maxFees = await getGasFees();
+    let r = await cSLASHER.slash(tokenIn, tokenTrade, weiIn, routerAddress1, routerAddress2, {maxFeePerGas: maxFees[0], maxPriorityFeePerGas: maxFees[1]});
+    return r.hash;
+}
+
+export async function getWeiOut(tokenIn, tokenOut, weiIn, routerAddress) {
+    return cSLASHER.getWeiOutMin(tokenIn, tokenOut, weiIn, routerAddress);
+}
+
+// export async function deploy() {
+//     let cf = await new ethers.ContractFactory(ISlasher, BCSlasher, wallet).deploy({gasPrice: (await provider.getFeeData()).gasPrice, chainId: 137, nonce: await provider.getTransactionCount(wallet.address), gasLimit: (await provider.getBlock('latest')).gasLimit});
+//     alert(cf.address);
+//     console.log(cf);
+// }
---
+++ src/index.js
@@ -0,0 +1,142 @@
+import { utils } from 'ethers';
+import { slasher, getWeiOut, preRun, deploy} from './chain.js';
+
+/* ERRORS
+    if there's an error in the transaction that used gas, the bot will pause to protect from future gas drains.
+    to inspect errors, right click on the page, click inspect, and click the console tab.
+    
+    POSSIBLE ERRORS
+        bad token/router:
+            the liquidity pair doesn't exist on one of the routers
+            check alchemy that the api key is still live
+        unlucky prediction of next gas price (spikes in gas fees might trigger this error for 1 block):
+            gas too low: will revert the transaction (rare because of tolerance cushion)
+            gas too high: possible to miss a small arbitrage
+        slower arbitrage: 
+            gwei set to fast (more gas than standard) but someone who pays more gas can take the opportunity (consumes your gas).
+*/
+
+// /* EDIT */
+let _tokensIn = ['0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', + '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270']; //wmatic prefered since most liquidity pools connect to wmatic
+let _amtBorrowed = ['100', '10']; //must be paired with tokensIn. (ex: borrow 100 wmatic, borrow 10 wmatic)
+let _tokensTraded = ['0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063' , '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6', '0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a', '0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39', '0x0B220b82F3eA3B7F6d9A1D8ab58930C064A2b5Bf', '0x101a023270368c0d50bffb62780f4afd4ea79c35', '0xe0339c80ffde91f3e20494df88d4206d86024cdf', '0x1B815d120B3eF02039Ee11dC2d33DE7aA4a8C603', '0x61299774020dA444Af134c82fa83E3810b309991', '0x8765f05ADce126d70bcdF1b0a48Db573316662eB', '0x8a2870fb69A90000D6439b7aDfB01d4bA383A415', '0xAcD7B3D9c10e97d0efA418903C0c7669E702E4C0', '0xb0897686c545045aFc77CF20eC7A532E3120E0F1'];
+let _routers = ['0x1b02da8cb0d097eb8d57a175b88c7d8b47997506', '0x3a1D87f206D12415f5b0A33E786967680AAb4f6d', '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff', '0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607']; //ex: quickswap router
+// /* EDIT */
+
+/*  HELP:
+    CTRL + S: saves and resets document. Must be done to confirm edits.
+    tokensIn and tokensTraded should have a liquidity pair on all routers (DAI/WMATIC pairs well but AAVE/LINK might not)
+*/
+
+/* OUTPUT LOG NOTATION
+    0: token borrowed
+    1: token traded
+    2: wei borrowed
+    3: wei out
+    4: router 1
+    5: router 2
+*/
+
+var arbitrage = {
+    'tokensIn': _tokensIn,
+    'tokensTraded': _tokensTraded,
+    'amtBorrowed': _amtBorrowed,
+    'routers': _routers
+}
+var paused = true;
+var loopCounter = 0;
+var arbitrageCounter = 0;
+var txHashes = '';
+
+async function checkArb(tokenIn, tokenTraded, weiBorrowed, router1, router2) {
+    let w1 = getWeiOut(tokenIn, tokenTraded, weiBorrowed, router1);
+    let w2 = getWeiOut(tokenTraded, tokenIn, w1, router2);
+    return [tokenIn, tokenTraded, weiBorrowed, w2, router1, router2]; 
+}
+
+async function findArb(arbitrage) {
+    let data = [];
+    let maxProfit = -2^15;
+    for (let h=0; h < arbitrage.tokensIn.length; h++) {
+        for(let i=0; i < arbitrage.tokensTraded.length; i++) {
+            for(let j=0; j < arbitrage.routers.length; j++) {
+                for(let k=0; k < arbitrage.routers.length; k++) {
+                    if (j!=k && arbitrage.tokensIn[h] != arbitrage.tokensTraded[i]) {
+                        data.push(checkArb(arbitrage.tokensIn[h], arbitrage.tokensTraded[i], utils.parseEther(arbitrage.amtBorrowed[h]), arbitrage.routers[j], arbitrage.routers[k]));
+                    }
+                }
+            }
+        }
+    }
+
+    for (let i=0; i < data.length; i++) {
+        let di;
+        let wOut;
+        try {
+            di = await data[i];
+            wOut = await di[3];
+            di[3] = wOut;
+        } catch {
+            console.log('ERROR BELOW: tokens do not have liquidity pair on at least one of these routers');
+        }
+
+        let profit = (wOut - di[2]) / di[2] * 100;
+        if (profit > maxProfit) {
+            maxProfit = profit;
+            if(maxProfit < 0) {
+                document.getElementById('maxProfit').style.color = 'red';
+            } else {
+                document.getElementById('maxProfit').style.color = 'green';
+            }
+            document.getElementById('maxProfit').innerHTML = 'maximum profit this loop: ' + maxProfit.toFixed(6) + ' %';
+        }
+
+        console.log(di);
+        console.log('profit: ' + profit + '%');
+        
+        if(profit > 0) {
+            if (wOut - di[2] > await preRun(di[0], di[1], di[2], di[4], di[5])) {
+                try {
+                    let txHash;
+                    arbitrageCounter++;
+                    document.getElementById('arbitrageCounter').innerHTML = 'arbitrages found: ' + arbitrageCounter;
+                    txHash = await slasher(di[0], di[1], di[2], di[4], di[5]);
+                    txHashes += '<br>' + txHash;
+                    document.getElementById('txHash').innerHTML = txHashes;
+                    console.log('SUCCESSFUL ARBITRAGE ABOVE');
+                } catch (er) {
+                    console.log(er);
+                    console.log('ERROR ABOVE: arbitrage failed');
+                    paused = true;
+                    alert('bot paused, arbitrage failed');
+                    break;
+                }
+            }
+        }
+    }
+}
+
+async function looper() {
+    loopCounter = 0;
+    arbitrageCounter = 0;
+    while (!paused) {
+        await findArb(arbitrage);
+        loopCounter++;
+        document.getElementById('loopCounter').innerHTML = 'loop iterations: ' + loopCounter;
+    }
+}
+
+async function toggleSlash() {
+    if (paused) {
+        paused = false;
+        looper();
+    }
+    else {
+        paused = true;
+        alert('bot will pause after loop (refresh page to force stop)');
+    }
+}
+
+//EVENTS
+document.getElementById('toggleSlash').addEventListener("click", e => {toggleSlash()});
+// document.getElementById('deploy').addEventListener("click", async() => {await deploy()});
